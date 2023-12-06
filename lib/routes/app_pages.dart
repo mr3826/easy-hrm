688c08f9023ea91634a09027ff4df01ab97d010b
lutfur <lutfur@gain.media>
2023-12-06 12:29:30 +0600

    route check

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -15,7 +15,7 @@ part 'app_routes.dart';
 
 class AppPages {
   AppPages._();
-  static const INITIAL = Routes.SPLASH_SCREEN;
+  static const INITIAL = Routes.MAIN_SCREEN;
 
   static final routes = [
     GetPage(
