07e8c2a8cb8e7fb71b66a98328c91cd2306a9352
dev-Evan <evan@gain.media>
2023-12-19 18:41:23 +0600

    logout api string added

--- lib/utils/api_endpoints.dart
+++ lib/utils/api_endpoints.dart
@@ -8,6 +8,7 @@ class Api {
 
   static const COMPANY_DOMAIN = "/organization";
   static const LOGIN = "/auth/login";
+  static const LOGOUT = "/auth/logout";
   static const FORGOT_PASSWORD = "/auth/forgot-password";
   static const RESEND_OTP = "/auth/resend-verification-code";
   static const RESET_PASSWORD = "/auth/verify-forgot-password-code";
