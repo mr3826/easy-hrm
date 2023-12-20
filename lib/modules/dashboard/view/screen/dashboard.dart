edbfbb24c773f4116387619f6320ae39d31d1771
lutfur <lutfur@gain.media>
2023-12-20 11:45:18 +0600

    logout development done

--- lib/modules/dashboard/view/screen/dashboard.dart
+++ lib/modules/dashboard/view/screen/dashboard.dart
@@ -21,9 +21,11 @@ import 'package:payrun_mobile/utils/utils.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 
 import '../../../../common/widget/custom_buttom_sheet.dart';
+import '../../../../common/widget/custom_network_image.dart';
 import '../../../../common/widget/custom_status_button.dart';
 import '../../../../utils/api_endpoints.dart';
 import '../../../leave/view/widget/leave_record_details_view.dart';
+import '../../../profile/controller/user_profile_controller.dart';
 
 class Dashboard extends GetView<DashboardController> {
   Dashboard({super.key});
@@ -275,23 +277,7 @@ class Dashboard extends GetView<DashboardController> {
   _userInfoAppbarLayout() {
     return Row(
       children: [
-        controller.profileSummaryForDashboard?.getProfileSummaryForDashboard
-                    ?.profile?.image !=
-                null
-            ? CircleAvatar(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.green,
-                radius: 37,
-                backgroundImage: NetworkImage(
-                    "${Api.PUBLIC_IMAGE_URL_DOMAIN}/files/${GetStorage().read(AppString.ORGANIZATION_ID)}/${controller.profileSummaryForDashboard?.getProfileSummaryForDashboard?.profile?.image}"),
-                child: const CupertinoActivityIndicator(),
-              )
-            : CircleAvatar(
-                backgroundColor: Colors.white,
-                foregroundColor: Colors.green,
-                radius: 37,
-                backgroundImage: AssetImage(Images.user),
-              ),
+        _userImageLayout(),
         customSpacerWidth(width: 12),
         Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -313,6 +299,15 @@ class Dashboard extends GetView<DashboardController> {
           ],
         )
       ],
+    );
+  }
+
+  _userImageLayout() {
+    return CustomNetworkImage(
+      height: 22,
+      imgUrl:
+          "${Api.PUBLIC_IMAGE_URL_DOMAIN}/files/${GetStorage().read(AppString.ORGANIZATION_ID)}/${controller.profileSummaryForDashboard?.getProfileSummaryForDashboard?.profile?.image}",
+      borderColor: Colors.transparent,
     );
   }
 
@@ -472,8 +467,7 @@ class Dashboard extends GetView<DashboardController> {
                   ? "| ${controller.upcommingLeaveDashboard?.getUpcomingLeavesForApp?[index].numberOfDays} days"
                   : "| ${controller.upcommingLeaveDashboard?.getUpcomingLeavesForApp?[index].numberOfDays} day",
           style: AppStyle.mid_large_text.copyWith(
-              color: AppColor.hintColor,
-              fontSize: Dimensions.fontSizeDefault),
+              color: AppColor.hintColor, fontSize: Dimensions.fontSizeDefault),
         )
       ],
     );
