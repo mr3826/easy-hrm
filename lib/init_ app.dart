5b080bda8830a75703e1c9963867571e2dddce7e
lutfur <lutfur@gain.media>
2024-02-06 18:41:36 +0600

    update timeline null check added

--- lib/init_ app.dart
+++ lib/init_ app.dart
@@ -1,24 +1,18 @@
-import 'package:payrun_mobile/modules/notification/controller/notification_controller.dart';
 import 'package:payrun_mobile/modules/profile/controller/password_controller.dart';
-import 'package:payrun_mobile/modules/leave/controller/leave_record_controller.dart';
-import 'package:payrun_mobile/modules/leave/controller/leave_screen_controller.dart';
 import 'package:payrun_mobile/modules/profile/controller/update_profile_controller.dart';
-import 'package:payrun_mobile/modules/profile/controller/user_profile_controller.dart';
-import 'package:payrun_mobile/modules/starting/controller/splash_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:payrun_mobile/modules/timeline/controller/selected_task_controller.dart';
-import 'package:payrun_mobile/modules/timeline/controller/timeline_controller.dart';
 import 'common/controller/date_time_controller.dart';
 import 'modules/auth/presentation/controller/forgot_password_controller.dart';
-import 'modules/dashboard/controller/dashbpard_controller.dart';
 import 'modules/profile/controller/log_out_controller.dart';
 import 'modules/profile/controller/profile_image_selected_controller.dart';
 import 'modules/leave/controller/calendar_date_controller.dart';
 import 'modules/leave/controller/file_upload_controller.dart';
 import 'modules/leave/controller/picked_file_form_storage.dart';
+import 'modules/starting/controller/splash_controller.dart';
 
 Future<void> initApp() async {
   await GetStorage.init();
@@ -38,4 +32,6 @@ Future<void> initApp() async {
   Get.put(SelectedTaskController());
   Get.put(UpdateProfileController());
   Get.put(DateTimeController());
+  Get.put(SplashController());
+
 }
