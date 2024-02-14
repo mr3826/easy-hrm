ab206f75b6be6aff6303e2f69caa2e2f8f37dd31
lutfur <lutfur@gain.media>
2024-02-14 16:21:56 +0600

    leave button sheet overflow fixed

--- lib/utils/app_string.dart
+++ lib/utils/app_string.dart
@@ -41,7 +41,7 @@ class AppString {
   static String text_remove_photo = 'Remove photo';
   static String text_back_to_login = 'Back to login';
   static String text_done_of_save = 'Done and Save';
-  static String text_we_have_a_verification_etc = 'We have sent a Verification code to your email address.Enter this code bellow to verify your account.';
+  static String text_we_have_a_verification_etc = 'We have sent a Verification code to your email address. Enter this code bellow to verify your account.';
   static String text_dont_not_worry =
       'Don’t worry! It happens. Please enter the email address associated with your account';
   static String text_associated = 'address associated with your account';
