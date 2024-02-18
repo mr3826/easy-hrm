bf1bc17592e34a63afbe556e3d735f35ba1834b0
dev-Evan <evan@gain.media>
2024-02-18 19:53:16 +0600

    notification context added

--- lib/utils/language/english.dart
+++ lib/utils/language/english.dart
@@ -2,6 +2,8 @@ import '../app_string.dart';
 
 Map<String, String> eng = {
 
+
+  ///leave and timlog approver,departmentHeaDText
   AppString.leaveAddedSuccessMessage: "Leave Added Successfully",
   AppString.dateDifferenceIssueMessage: "In time must be before out time",
   AppString.text_stitch_org: "Switch Organization",
