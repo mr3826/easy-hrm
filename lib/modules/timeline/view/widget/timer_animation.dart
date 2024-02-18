3a5a2fcdb4f22941ba9199efea882768a06e1ff3
lutfur <lutfur@gain.media>
2024-02-18 16:54:51 +0600

    height chnage for horizontal line

--- lib/modules/timeline/view/widget/timer_animation.dart
+++ lib/modules/timeline/view/widget/timer_animation.dart
@@ -22,11 +22,12 @@ class TimerAnimation extends GetView<TimeCounterController> {
             controller.isTotalCount.value == false
                 ? _totalCountContainer()
                 : _normalContainer(),
-            Positioned(
-                bottom: -140,
-                left: -140,
-                right: -140,
-                child: _horizontalLineAnimation()),
+            Obx(() => (Get.find<TimeCounterController>().isRunning.isTrue &&
+                    Get.find<TimeCounterController>()
+                        .isRunningHorizontalLine
+                        .isTrue)
+                ? _horizontalLineAnimation()
+                : _nullHorizontalLineLayout()),
           ],
         ));
   }
@@ -103,31 +104,34 @@ class TimerAnimation extends GetView<TimeCounterController> {
   }
 
   _horizontalLineAnimation() {
-    return Obx(
-      () => (Get.find<TimeCounterController>().isRunning.isTrue &&
-              Get.find<TimeCounterController>().isRunningHorizontalLine.isTrue)
-          ? Lottie.asset(Images.timer_animation,
-              height: 300, width: 300, fit: BoxFit.fitHeight)
-          : Padding(
-              padding: const EdgeInsets.only(
-                bottom: 70.0,
-              ),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                    4,
-                    (index) => Padding(
-                          padding: const EdgeInsets.all(4.0),
-                          child: Icon(
-                            Icons.circle,
-                            color: AppColor.cardColor.withOpacity(0.2),
-                            size: 12,
-                          ),
-                        )),
-              ),
-            ),
+    return Positioned(
+        bottom: -140,
+        left: -140,
+        right: -140,
+        child: Lottie.asset(Images.timer_animation,
+            height: 300, width: 300, fit: BoxFit.fitHeight));
+  }
+
+  _nullHorizontalLineLayout() {
+    return Positioned(
+      bottom: -0,
+      left: -50,
+      right: -50,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: List.generate(
+            4,
+            (index) => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Icon(
+                    Icons.circle,
+                    color: AppColor.cardColor.withOpacity(0.2),
+                    size: 25,
+                  ),
+                )),
+      ),
     );
   }
 }
