ef6c484876af9add0a6f4f27d22cc59b42994c3a
lutfur <lutfur@gain.media>
2024-02-19 19:13:50 +0600

    add new entry bug fixed

--- lib/modules/timeline/view/widget/floating_btn_layout.dart
+++ lib/modules/timeline/view/widget/floating_btn_layout.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:payrun_mobile/common/widget/custom_spacer.dart';
 import 'package:payrun_mobile/utils/app_color.dart';
 import 'package:payrun_mobile/utils/app_layout.dart';
@@ -20,31 +21,62 @@ Widget floatingButton(
             borderRadius: BorderRadius.circular(Dimensions.radiusExtraLarge)),
         height: AppLayout.getHeight(46),
         width: double.infinity,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Center(
-                child: Icon(
-              icon ?? Icons.add,
-              color: AppColor.cardColor,
-              size: 17,
-            )),
-            customSpacerWidth(width: 4),
-            SizedBox(
-                width: MediaQuery.of(Get.context!).size.width / 5,
-                child: Center(
-                    child: Text(
-                  btnText,
-                  style: AppStyle.mid_large_text.copyWith(
-                      color: AppColor.cardColor,
-                      fontWeight: FontWeight.w700,
-                      fontSize: Dimensions.radiusMid - 1,
-                      overflow: TextOverflow.ellipsis),
-                ))),
-          ],
-        ),
+        child: GetStorage().read("languageCode") != null &&
+                (GetStorage().read("languageCode")) == "no"
+            ? _languageNotNullLayout(icon, btnText)
+            : Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(
+                    icon ?? Icons.add,
+                    color: AppColor.cardColor,
+                    size: 17,
+                  ),
+                  customSpacerWidth(width: 4),
+                  Text(
+                    btnText,
+                    style: AppStyle.mid_large_text.copyWith(
+                        color: AppColor.cardColor,
+                        fontWeight: FontWeight.w700,
+                        fontSize: Dimensions.radiusMid - 1,
+                        overflow: TextOverflow.ellipsis),
+                  ),
+                ],
+              ),
       ),
+    ),
+  );
+}
+
+_languageNotNullLayout(icon, btnText) {
+  return Padding(
+    padding: const EdgeInsets.all(5.0),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Spacer(),
+        Center(
+          child: Icon(
+            icon ?? Icons.add,
+            color: AppColor.cardColor,
+            size: 17,
+          ),
+        ),
+        customSpacerWidth(width: 4),
+        Expanded(
+            flex: 5,
+            child: Text(
+              btnText,
+              style: AppStyle.mid_large_text.copyWith(
+                  color: AppColor.cardColor,
+                  fontWeight: FontWeight.w700,
+                  fontSize: Dimensions.radiusMid - 1,
+                  overflow: TextOverflow.ellipsis),
+            )),
+        const Spacer(),
+      ],
     ),
   );
 }
