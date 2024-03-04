67cad0825609f69abf3b05568ae7c64dd1fccd27
dev-Evan <evan@gain.media>
2024-03-04 12:39:07 +0600

    org switch data change accordingly

--- lib/init_ app.dart
+++ lib/init_ app.dart
@@ -10,6 +10,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:payrun_mobile/modules/timeline/controller/selected_task_controller.dart';
+import 'package:payrun_mobile/modules/timeline/controller/timelog_summary_controller.dart';
 import 'common/controller/date_time_controller.dart';
 import 'common/controller/language_controller.dart';
 import 'firebase_options.dart';
@@ -58,4 +59,5 @@ Future<void> initApp() async {
   Get.put(LeaveRecordsController());
   Get.put(DateTimeController());
   Get.put(SplashController());
+  Get.put(TimelineSummaryController());
 }
