894af3072322e8366e20dc0e895eaf60a0837284
lutfur <lutfur@gain.media>
2024-12-17 14:15:37 +0600

    add profile helper class

--- lib/modules/profile/view/screen/profile_v2/profile_main_screen.dart
+++ lib/modules/profile/view/screen/profile_v2/profile_main_screen.dart
@@ -7,6 +7,8 @@ class ProfileMainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
--- lib/modules/profile/view/screen/user_profile.dart
+++ lib/modules/profile/view/screen/user_profile.dart
@@ -1,237 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-// import 'package:payrun_mobile/modules/profile/model/employee_work_history.dart';
-// import 'package:payrun_mobile/modules/profile/view/widget/department_layout_widget.dart';
-// import 'package:payrun_mobile/modules/profile/view/widget/employee_stauts_layout.dart';
-// import '../../../../common/widget/custom_drawer.dart';
-// import '../../../../common/widget/custom_spacer.dart';
-// import '../../../../common/widget/loading_indicator.dart';
-// import '../../../../utils/app_color.dart';
-// import '../../../../utils/app_string.dart';
-// import '../../../../utils/app_style.dart';
-// import '../../../../utils/dimensions.dart';
-// import '../../../auth/presentation/view/otp_screen.dart';
-// import '../../controller/user_profile_controller.dart';
-// import '../widget/chnage_email_notify_layout.dart';
-// import '../widget/common_widget.dart';
-// import 'custom/custom.dart';
-//
-// class ProfileScreen extends GetView<UserProfileController> {
-//   const ProfileScreen({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final screenHeight = MediaQuery.sizeOf(context).height;
-//     final screenWidth = MediaQuery.sizeOf(context).width;
-//
-//
-//     return  controller.obx((sate)=>Scaffold(
-//       backgroundColor: AppColor.primaryColor,
-//       body: Stack(
-//         children: [
-//           _buildBackgroundContainer(context),
-//           _buildProfileImage(screenHeight, screenWidth),
-//         ],
-//       ),
-//     ),onLoading: const LoadingIndicator());
-//
-//   }
-//
-//   /// Background Container with Profile Layout
-//   Widget _buildBackgroundContainer(BuildContext context) {
-//     return Container(
-//       width: double.infinity,
-//       decoration: const BoxDecoration(
-//         color: AppColor.primaryColor,
-//         borderRadius: BorderRadiusDirectional.only(
-//           topEnd: Radius.circular(25),
-//           topStart: Radius.circular(25),
-//         ),
-//       ),
-//       child: Padding(
-//         padding: const EdgeInsets.only(top: 50.0),
-//         child: Column(
-//           children: [
-//             _buildHeader(context),
-//             const SizedBox(height: 10),
-//             _buildContent(context),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//   /// Positioned Profile Image
-//   Widget _buildProfileImage(double screenHeight, double screenWidth) {
-//     return Positioned(
-//       top: screenHeight * 0.11,
-//       left: screenWidth * 0.05,
-//       right: screenWidth * 0.05,
-//       child: userImageLayout(),
-//     );
-//   }
-//
-//
-//
-//   /// Profile Header with Title and Menu Button
-//   Widget _buildHeader(BuildContext context) {
-//     return SizedBox(
-//       height: 70,
-//       child: Padding(
-//         padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 16),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             Text(
-//               AppString.text_profile.tr,
-//               style: AppStyle.mid_large_text.copyWith(
-//                 color: AppColor.cardColor,
-//                 fontWeight: FontWeight.w600,
-//                 fontSize: Dimensions.fontSizeMid + 1,
-//               ),
-//             ),
-//             IconButton(
-//               onPressed: () {
-//                 showCustomDrawer(
-//                   context: context,
-//                   child: Container(
-//                     color: Colors.transparent,
-//                     width: double.infinity,
-//                     child: endDrawer(context),
-//                   ),
-//                 );
-//               },
-//               icon: const Icon(Icons.menu, color: AppColor.cardColor),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//
-//
-//
-//   /// Main Content Container
-//   Widget _buildContent(BuildContext context) {
-//     return Expanded(
-//       child: Container(
-//         width: double.infinity,
-//         decoration: const BoxDecoration(
-//           color: AppColor.cardColor,
-//           borderRadius: BorderRadiusDirectional.only(
-//             topEnd: Radius.circular(30),
-//             topStart: Radius.circular(30),
-//           ),
-//         ),
-//         child: Padding(
-//           padding: marginLayout,
-//           child: Column(
-//             children: [
-//               customSpacerHeight(height: 50),
-//
-//               /// User info section
-//               userInfoLayout(context),
-//               customSpacerHeight(height: 30),
-//
-//               /// RefreshIndicator with scrollable content
-//               Expanded(
-//                 child: RefreshIndicator(
-//                   onRefresh: _fetchProfileData, // Call the refresh method
-//                   child: SingleChildScrollView(
-//                     physics: const AlwaysScrollableScrollPhysics(),
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.start,
-//                       children: [
-//                         /// Monthly layout
-//                         monthlyStatusLayout(),
-//
-//                         customSpacerHeight(height: 25),
-//                         /// User description
-//                         descriptionTextLayout(),
-//
-//                         customSpacerHeight(height: 8),
-//
-//                         /// User email
-//                         const BuildEmail(),
-//
-//                         /// Phone number
-//                         _buildPhoneNumberSection(),
-//
-//                         /// Employee address
-//                         addressText(),
-//                         customSpacerHeight(height: 15),
-//
-//                         /// Department layout
-//                         _buildDepartmentLayout(context),
-//
-//                         customSpacerHeight(height: 5),
-//
-//                         /// Designation history
-//                         _buildDesignationHistoryLayout(context),
-//                         customSpacerHeight(height: 50),
-//                       ],
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   /// Fetches the latest profile data from the server.
-//   Future<void> _fetchProfileData() async {
-//     try {
-//       await controller.getUserProfile();
-//       await controller.getEmploymentInfo();
-//       await controller.getUserLogHistory();
-//       await controller.getOrganizationInfo();
-//     } catch (e) {
-//       // Optionally handle errors or show a message
-//       Get.snackbar('Error', 'Failed to refresh data');
-//     }
-//   }
-//
-//   Widget _buildPhoneNumberSection() {
-//     return Column(
-//       crossAxisAlignment: CrossAxisAlignment.start,
-//       children: [
-//         customSpacerHeight(height: 15),
-//         phoneNumberText(),
-//         customSpacerHeight(height: 15),
-//         emergencyPhoneNumber(),
-//         customSpacerHeight(height: 15),
-//       ],
-//     );
-//   }
-//
-//   Widget _buildDepartmentLayout(BuildContext context) {
-//     List<DeptHistories>? departmentHistory = controller.employeeWorkHistory
-//         ?.getOrganizationUserHistory
-//         ?.deptHistories;
-//     if (departmentHistory != null && departmentHistory.isNotEmpty) {
-//       return departmentLayout(context);
-//     }
-//     return const SizedBox.shrink();
-//   }
-//
-//   Widget _buildDesignationHistoryLayout(BuildContext context) {
-//     List<DesignationHistories>? designationHistory = controller
-//         .employeeWorkHistory
-//         ?.getOrganizationUserHistory
-//         ?.designationHistories;
-//     if (designationHistory != null && designationHistory.isNotEmpty) {
-//       return employeeStatusLayout(context: context);
-//     }
-//     return const SizedBox.shrink();
-//   }
-//
-//
-// }
-//
-//
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:payrun_mobile/modules/profile/model/employee_work_history.dart';
@@ -248,7 +14,6 @@ import '../../../auth/presentation/view/otp_screen.dart';
 import '../../controller/user_profile_controller.dart';
 import '../widget/chnage_email_notify_layout.dart';
 import '../widget/common_widget.dart';
-import 'custom/custom.dart';
 
 class ProfileScreen extends GetView<UserProfileController> {
   const ProfileScreen({super.key});
@@ -258,33 +23,17 @@ class ProfileScreen extends GetView<UserProfileController> {
     final screenHeight = MediaQuery.sizeOf(context).height;
     final screenWidth = MediaQuery.sizeOf(context).width;
 
-    return controller.obx(
-        (sate) => Scaffold(
-              backgroundColor: AppColor.primaryColor,
-              body: ProfileScreen1(
-                userInformation: UserInformation(
-                    userName: "${controller.userDetails?.getOrganizationUserDetails?.profile?.firstName ?? ""} ${controller.userDetails?.getOrganizationUserDetails?.profile?.lastName ?? ""}",
-                    departmentName: controller.userDetails?.getOrganizationUserDetails?.department?.name ??"",
-                  employeeId: controller.userDetails?.getOrganizationUserDetails?.employeeId ??"",
-                  employmentStatus: [controller.userDetails?.getOrganizationUserDetails?.status],
 
-                  leaveBalance: controller.userLogHistory
-                      ?.geTimelogAndLeaveAvailabilityForApp?.balanceLeave.toString() ??
-                      "",
+    return  controller.obx((sate)=>Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      body: Stack(
+        children: [
+          _buildBackgroundContainer(context),
+          _buildProfileImage(screenHeight, screenWidth),
+        ],
+      ),
+    ),onLoading: const LoadingIndicator());
 
-                  monthGoal: controller.userLogHistory
-                      ?.geTimelogAndLeaveAvailabilityForApp?.totalSchedule ??
-                      "",
-
-                  loggedTime: controller.userLogHistory
-                      ?.geTimelogAndLeaveAvailabilityForApp?.totalLogged ??
-                      "",
-
-
-                ),
-              ),
-            ),
-        onLoading: const LoadingIndicator());
   }
 
   /// Background Container with Profile Layout
@@ -310,7 +59,6 @@ class ProfileScreen extends GetView<UserProfileController> {
       ),
     );
   }
-
   /// Positioned Profile Image
   Widget _buildProfileImage(double screenHeight, double screenWidth) {
     return Positioned(
@@ -320,6 +68,8 @@ class ProfileScreen extends GetView<UserProfileController> {
       child: userImageLayout(),
     );
   }
+
+
 
   /// Profile Header with Title and Menu Button
   Widget _buildHeader(BuildContext context) {
@@ -357,6 +107,8 @@ class ProfileScreen extends GetView<UserProfileController> {
     );
   }
 
+
+
   /// Main Content Container
   Widget _buildContent(BuildContext context) {
     return Expanded(
@@ -392,7 +144,6 @@ class ProfileScreen extends GetView<UserProfileController> {
                         monthlyStatusLayout(),
 
                         customSpacerHeight(height: 25),
-
                         /// User description
                         descriptionTextLayout(),
 
@@ -455,8 +206,9 @@ class ProfileScreen extends GetView<UserProfileController> {
   }
 
   Widget _buildDepartmentLayout(BuildContext context) {
-    List<DeptHistories>? departmentHistory = controller
-        .employeeWorkHistory?.getOrganizationUserHistory?.deptHistories;
+    List<DeptHistories>? departmentHistory = controller.employeeWorkHistory
+        ?.getOrganizationUserHistory
+        ?.deptHistories;
     if (departmentHistory != null && departmentHistory.isNotEmpty) {
       return departmentLayout(context);
     }
@@ -465,10 +217,17 @@ class ProfileScreen extends GetView<UserProfileController> {
 
   Widget _buildDesignationHistoryLayout(BuildContext context) {
     List<DesignationHistories>? designationHistory = controller
-        .employeeWorkHistory?.getOrganizationUserHistory?.designationHistories;
+        .employeeWorkHistory
+        ?.getOrganizationUserHistory
+        ?.designationHistories;
     if (designationHistory != null && designationHistory.isNotEmpty) {
       return employeeStatusLayout(context: context);
     }
     return const SizedBox.shrink();
   }
+
+
 }
+
+
+
