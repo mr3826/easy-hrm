e725ba3732d00ed39e22e93bab7b6c45a02cc41e
lutfur <lutfur@gain.media>
2025-01-09 14:15:06 +0600

    timeline calender updated [timeline v2]

--- lib/app/modules/hr_timeline/view/screen/hr_timeline_screen.dart
+++ lib/app/modules/hr_timeline/view/screen/hr_timeline_screen.dart
@@ -21,6 +21,7 @@ import '../../controllers/start_timer_controller.dart';
 import '../widgets/time_sheet/build_select_month.dart';
 import '../widgets/timeline_calender/build_hr_timeline_calendar.dart';
 
+bool isEmployee = false;
 
 class HrTimelineScreen extends StatefulWidget {
   const HrTimelineScreen({super.key});
@@ -29,19 +30,20 @@ class HrTimelineScreen extends StatefulWidget {
   State<HrTimelineScreen> createState() => _HrTimelineScreenState();
 }
 
-class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerProviderStateMixin {
+class _HrTimelineScreenState extends State<HrTimelineScreen>
+    with SingleTickerProviderStateMixin {
   late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: 2, vsync: this);
+    _tabController =
+        TabController(length: isEmployee == true ? 1 : 2, vsync: this);
     _tabController.addListener(() {
       if (_tabController.indexIsChanging) {
         setState(() {}); // Rebuild the widget when the tab changes
       }
     });
-
   }
 
   @override
@@ -49,18 +51,20 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
     _tabController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     Get.put(StartTimerController());
+
     return DefaultTabController(
       length: 2, // Number of tabs
       child: Scaffold(
         body: CustomScrollView(
           slivers: [
-            _sliverAppbar(_tabController),
+            isEmployee == true
+                ? _isEmployeeSilverAppbar
+                : _isAdminSilverAppbar(_tabController),
             _sliverAppbarBody(_tabController),
-
-
           ],
         ),
         floatingActionButton: _timerBtnLayout(context),
@@ -68,7 +72,7 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
     );
   }
 
-  _sliverAppbar(TabController tabController) {
+  _isAdminSilverAppbar(TabController tabController) {
     return SliverAppBar(
       backgroundColor: AppColor.cardColor,
       foregroundColor: AppColor.cardColor,
@@ -85,21 +89,21 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
               Padding(
                 padding: const EdgeInsets.only(top: 22.0),
                 child: SizedBox(
-                  height: 40,
+                  height: AppLayout.getHeight(40),
                   child: TabBar(
                     labelColor: AppColor.cardColor,
                     controller: tabController,
                     unselectedLabelColor: AppColor.normalTextColor,
-                    unselectedLabelStyle: AppStyle.normal_text.copyWith(
-                        fontSize: Dimensions.fontSizeDefault + 1),
+                    unselectedLabelStyle: AppStyle.normal_text
+                        .copyWith(fontSize: Dimensions.fontSizeDefault + 1),
                     indicator: BoxDecoration(
                       borderRadius: BorderRadius.circular(6),
                       color: AppColor.primaryColor,
                     ),
                     indicatorPadding: EdgeInsets.zero,
                     indicatorSize: TabBarIndicatorSize.tab,
-                    labelStyle: AppStyle.normal_text.copyWith(
-                        fontSize: Dimensions.fontSizeDefault + 1),
+                    labelStyle: AppStyle.normal_text
+                        .copyWith(fontSize: Dimensions.fontSizeDefault + 1),
                     tabs: [
                       Tab(text: AppString.textCalendar.tr),
                       Tab(text: AppString.text_time_sheet.tr),
@@ -118,17 +122,48 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
     );
   }
 
-  _sliverAppbarBody(TabController tabController) {
-    return   SliverList(
-        delegate: SliverChildListDelegate([_tabController.index==0?
-        const ISEmployeeTimelineCalendar():const BuildTimeSheet()
-        ]));
+  SliverAppBar get _isEmployeeSilverAppbar {
+    return SliverAppBar(
+      expandedHeight: AppLayout.getHeight(284),
+      elevation: 0,
+      bottom: _buttonRadiusLayout(),
+      pinned: true,
+      backgroundColor: AppColor.primaryColor,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(left: 18, right: 18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  customSpacerHeight(height: 45),
+                  _timelineText(),
+                  customSpacerHeight(height: 12),
+                  timelineLayout(),
+                  customSpacerHeight(height: 6),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
-
-
-
-
+  _sliverAppbarBody(TabController tabController) {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        if (isEmployee)
+          const ISEmployeeTimelineCalendar()
+        else
+          tabController.index == 0
+              ? const ISEmployeeTimelineCalendar()
+              : const BuildTimeSheet(),
+      ]),
+    );
+  }
 
   _buildAppbar() {
     return Padding(
@@ -162,12 +197,12 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
                 topRight: Radius.circular(Dimensions.radiusMid + 15),
                 topLeft: Radius.circular(Dimensions.radiusMid + 15))),
         width: double.maxFinite,
-        padding: const EdgeInsets.only(top: 12, bottom: 5),
-        child: tabIndex==0? Obx(() => dateCalendarLayout()):const BuildSelectMonth(),
+        child: tabIndex == 0
+            ? Obx(() => dateCalendarLayout())
+            : const BuildSelectMonth(),
       ),
     );
   }
-
 
   _timerBtnLayout(BuildContext context) {
     final StartTimerController controller = Get.find<StartTimerController>();
@@ -179,7 +214,8 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
       child: Row(
         children: [
           controller.isRunning.value
-              ? _timerStringOpenBtn(time: controller.starTimeDashboard.toString())
+              ? _timerStringOpenBtn(
+                  time: controller.starTimeDashboard.toString())
               : _timerStringBtn(context),
           customSpacerWidth(width: 18),
           _addTimeEntryBtn(),
@@ -219,202 +255,26 @@ class _HrTimelineScreenState extends State<HrTimelineScreen> with SingleTickerPr
         btnText: time.toString());
   }
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-_silverAppbar(TabController _tabController ) {
-  var clickIndex=0;
-  print('Tab index (swipe or tap): ${_tabController.index}');
-
-  return SliverAppBar(
-    backgroundColor: AppColor.cardColor,
-    foregroundColor: AppColor.cardColor,
-    floating: true,
-    pinned: true,
-    expandedHeight: 248,
-    elevation: 0,
-    flexibleSpace: FlexibleSpaceBar(
-      background: Padding(
-        padding: const EdgeInsets.only(left: 18.0,right: 18),
-        child: Column(
-          children: [
-          //  _buildAppbar(),
-            Padding(
-              padding: const EdgeInsets.only(top: 22.0),
-              child: SizedBox(
-                height: 40,
-                child: TabBar(
-                  labelColor: AppColor.cardColor,
-                  controller: _tabController,
-                  unselectedLabelColor: AppColor.normalTextColor,
-                  unselectedLabelStyle: AppStyle.normal_text
-                      .copyWith(
-                      fontSize: Dimensions.fontSizeDefault + 1),
-                  indicator:  BoxDecoration(
-                    borderRadius: BorderRadius.circular(6),
-                    color: AppColor
-                        .primaryColor,
-                  ),
-                  indicatorPadding: EdgeInsets.zero,
-                  indicatorSize: TabBarIndicatorSize.tab,
-                  labelStyle: AppStyle.normal_text.copyWith(
-                      fontSize: Dimensions.fontSizeDefault + 1),
-                  tabs: [
-                    Tab(text: AppString.textCalendar.tr),
-                    Tab(text: AppString.text_time_sheet.tr),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const CustomSearchBar()
-          ],
-        ),
+  _buttonRadiusLayout() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(26),
+      child: Container(
+        decoration: BoxDecoration(
+            color: AppColor.backgroundColor,
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(Dimensions.radiusMid + 15),
+                topLeft: Radius.circular(Dimensions.radiusMid + 15))),
+        width: double.maxFinite,
+        padding: const EdgeInsets.only(top: 12, bottom: 5),
+        child: Obx(() => dateCalendarLayout()),
       ),
-    ),
-    centerTitle: true,
-    //bottom: _bottomLayout(0),
-  );
+    );
+  }
+
+  _timelineText() {
+    return Text(
+      AppString.text_time_line.tr,
+      style: AppStyle.mid_large_text.copyWith(fontSize: 20),
+    );
+  }
 }
-
-
-_sliverAppList(TabController tabController) {
-
-  // return   SliverFillRemaining(
-  //   child: TabBarView(
-  //     controller: _tabController,
-  //     children: [
-  //       ISEmployeeTimelineCalendar(),
-  //       BuildTimeSheet()
-  //
-  //     ],
-  //   ),
-  // );
-
-
-}
-
-
-SliverAppBar get sliverAppBar {
-  return SliverAppBar(
-    expandedHeight: AppLayout.getHeight(284),
-    elevation: 0,
-    bottom: _buttonRadiusLayout(),
-    pinned: true,
-    backgroundColor: AppColor.primaryColor,
-    flexibleSpace: FlexibleSpaceBar(
-      background: Padding(
-        padding: const EdgeInsets.only(left: 20, right: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                customSpacerHeight(height: 45),
-                _timelineText(),
-                customSpacerHeight(height: 12),
-                timelineLayout(),
-                customSpacerHeight(height: 6),
-              ],
-            ),
-          ],
-        ),
-      ),
-    ),
-  );
-}
-
-SliverList get sliverList {
-  return SliverList(
-    delegate: SliverChildListDelegate([
-
-
-      //const ISEmployeeTimelineCalendar()
-
-      TabBarView(
-        children: [
-          ISEmployeeTimelineCalendar(),
-          BuildTimeSheet()
-        ],
-      )
-
-    ]),
-  );
-}
-
-
-_isEmployeeView() {
-  return CustomScrollView(
-    slivers: [
-      sliverAppBar,
-     // sliverList,
-    ],
-  );
-}
-
-
-
-
-_buttonRadiusLayout() {
-  return PreferredSize(
-    preferredSize: const Size.fromHeight(26),
-    child: Container(
-      decoration: BoxDecoration(
-          color: AppColor.backgroundColor,
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(Dimensions.radiusMid + 15),
-              topLeft: Radius.circular(Dimensions.radiusMid + 15))),
-      width: double.maxFinite,
-      padding: const EdgeInsets.only(top: 12, bottom: 5),
-      child: Obx(() => dateCalendarLayout()),
-    ),
-  );
-}
-
-_timelineText() {
-  return Text(
-    AppString.text_time_line.tr,
-    style: AppStyle.mid_large_text.copyWith(fontSize: 20),
-  );
-}
-
-Widget _buildTabBarWithSearchSection() {
-  List<TabItem> tabs = [
-    TabItem(
-        label: AppString.textCalendar.tr,
-        body: const ISAdminTimelineCalendar()),
-    TabItem(label: AppString.text_time_sheet.tr, body: const BuildTimeSheet()),
-  ];
-
-  return TabBarWidget(
-    tabs: tabs,
-    onTabSelect: (index) {
-      print("Selected tab index: $index");
-    },
-  );
-}
-
-
