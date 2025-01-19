054917c67c012e5b7b3191a729fc02cd6ddb35ff
lutfur <lutfur@gain.media>
2025-01-19 14:37:50 +0600

    main_dev [flavor] add env.

--- lib/app/modules/hr_dashboard/services/dashboard_api_services.dart
+++ lib/app/modules/hr_dashboard/services/dashboard_api_services.dart
@@ -3,187 +3,183 @@ import 'package:payrun_mobile/app/global/services/api_service.dart';
 import 'package:payrun_mobile/utils/api_endpoints.dart';
 
 class DashBoardApiService {
- final ApiService _apiService;
+  final ApiService _apiService;
   DashBoardApiService(this._apiService);
 
   Future<Map<String, dynamic>?> getEmployeeOverView() async {
-    QueryResult<Object?> response= await _apiService.gqlCall(query: getEmployeeOverviewQuery);
-    return response.data;
+    QueryResult<Object?> response =
+        await _apiService.gqlCall(query: getEmployeeOverviewQuery);
+    return response.data; ///todo [one line]
   }
 
-  Future<Map<String, dynamic>?>  getJobOpening() async {
-
-
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getJobOpeningQuery,variables: {
+  Future<Map<String, dynamic>?> getJobOpening() async {
+    Map<String, Map<String, dynamic>> variables = {
       "queryData": {
-        "status": [
-          "published"
-        ],
-      },
-    });
+        "status": ["published"],
+      }
+    };
+
+    QueryResult<Object?> response =
+        await _apiService.gqlCall(query: getJobOpeningQuery, variables:variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  getLeaveAndTimeLogSummary() async {
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getLeaveAndTimeLogQuery);
+  Future<Map<String, dynamic>?> getLeaveAndTimeLogSummary() async {
+    QueryResult<Object?> response =
+        await _apiService.gqlCall(query: getLeaveAndTimeLogQuery);
     return response.data;
   }
 
-
-  Future<Map<String, dynamic>?>  getJobApplicationBoard(String entityId) async {
+  Future<Map<String, dynamic>?> getJobApplicationBoard(String entityId) async {
     Map<String, Map<String, dynamic>> variables = {
       "queryData": {
         "entity_id": entityId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getJobApplicationBoardQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getJobApplicationBoardQuery, variables: variables);
     return response.data;
   }
 
-
- Future<Map<String, dynamic>?>  updateJobApplication(String hiringStageId,String jobApplicationId) async {
+  Future<Map<String, dynamic>?> updateJobApplication(
+      String hiringStageId, String jobApplicationId) async {
     Map<String, Map<String, dynamic>> variables = {
       "inputData": {
         "hiring_stage_id": hiringStageId,
         "job_application_id": jobApplicationId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:updateJobApplicationQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: updateJobApplicationQuery, variables: variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  updateJob(String entityId) async {
+  Future<Map<String, dynamic>?> updateJob(String entityId) async {
     Map<String, Map<String, dynamic>> variables = {
-      "queryData": {
-        "entity_id": entityId
-      },
-      "updatingData": {
-        "status": "unpublished"
-      }
+      "queryData": {"entity_id": entityId},
+      "updatingData": {"status": "unpublished"}
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:updateJobQuery,variables: variables);
+    QueryResult<Object?> response =
+        await _apiService.gqlCall(query: updateJobQuery, variables: variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  removeJobApplication(String jobId,String candidateId) async {
+  Future<Map<String, dynamic>?> removeJobApplication(
+      String jobId, String candidateId) async {
     Map<String, Map<String, dynamic>> variables = {
       "inputData": {
         "job_id": jobId,
         "candidate_id": candidateId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:removeJobApplicationQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: removeJobApplicationQuery, variables: variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  createCandidateReview(String jobId,String jobApplicationId, int rate) async {
+  Future<Map<String, dynamic>?> createCandidateReview(
+      String jobId, String jobApplicationId, int rate) async {
     Map<String, Map<String, dynamic>> variables = {
       "inputData": {
         "job_id": jobId,
         "job_application_id": jobApplicationId,
-        "rate":rate
+        "rate": rate
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:createCandidateReviewQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: createCandidateReviewQuery, variables: variables);
     return response.data;
   }
 
-
-
-
-  Future<Map<String, dynamic>?>  createCandidateNoteReview(String jobId,String jobApplicationId, String note) async {
+  Future<Map<String, dynamic>?> createCandidateNoteReview(
+      String jobId, String jobApplicationId, String note) async {
     Map<String, Map<String, dynamic>> variables = {
       "inputData": {
         "job_id": jobId,
         "job_application_id": jobApplicationId,
-        "note":note
+        "note": note
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:createTeamNoteQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: createTeamNoteQuery, variables: variables);
     return response.data;
   }
 
-
-  Future<Map<String, dynamic>?>  deleteCandidateNoteReview(String entityId) async {
+  Future<Map<String, dynamic>?> deleteCandidateNoteReview(
+      String entityId) async {
     Map<String, Map<String, dynamic>> variables = {
       "inputData": {
         "entity_id": entityId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:deleteTeamNoteQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: deleteTeamNoteQuery, variables: variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  updateCandidateNoteReview(String noteId,String note) async {
+  Future<Map<String, dynamic>?> updateCandidateNoteReview(
+      String noteId, String note) async {
     Map<String, Map<String, dynamic>> variables = {
-      "inputData": {
-        "team_note_id": noteId,
-        "note":note
-      }
+      "inputData": {"team_note_id": noteId, "note": note}
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:updateTeamNoteQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: updateTeamNoteQuery, variables: variables);
     return response.data;
   }
 
-
-
-
-
-  Future<Map<String, dynamic>?>  getCandidateActivitiesLogs(String jobApplicationId) async {
+  Future<Map<String, dynamic>?> getCandidateActivitiesLogs(
+      String jobApplicationId) async {
     Map<String, Map<String, dynamic>> variables = {
       "queryData": {
         "job_application_id": jobApplicationId,
         "request_from": "job_application"
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getCandidateLogsQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getCandidateLogsQuery, variables: variables);
     return response.data;
   }
 
- Future<Map<String, dynamic>?>  getJobApplicationPreview(String jobId,String candidateId) async {
+  Future<Map<String, dynamic>?> getJobApplicationPreview(
+      String jobId, String candidateId) async {
     Map<String, Map<String, dynamic>> variables = {
-      "queryData": {
-        "candidate_id": candidateId,
-        "job_id": jobId
-      }
+      "queryData": {"candidate_id": candidateId, "job_id": jobId}
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getJobApplicationPreviewQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getJobApplicationPreviewQuery, variables: variables);
     print("getJobApplicationPreview_main:: $response");
 
     return response.data;
   }
 
-
-
-
-
-
-  Future<Map<String, dynamic>?>  getCandidateDetails(String jobApplicationId) async {
+  Future<Map<String, dynamic>?> getCandidateDetails(
+      String jobApplicationId) async {
     Map<String, Map<String, dynamic>> variables = {
       "queryData": {
         "job_application_id": jobApplicationId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getCandidateDetailsQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getCandidateDetailsQuery, variables: variables);
     return response.data;
   }
 
-  Future<Map<String, dynamic>?>  getCandidateReview(String jobApplicationId) async {
+  Future<Map<String, dynamic>?> getCandidateReview(
+      String jobApplicationId) async {
     Map<String, Map<String, dynamic>> variables = {
       "queryData": {
         "job_application_id": jobApplicationId,
       }
     };
-    QueryResult<Object?> response= await _apiService.gqlCall(query:getTeamNotesQuery,variables: variables);
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getTeamNotesQuery, variables: variables);
     return response.data;
   }
 
-
- Future<Map<String, dynamic>?>  getFileSignUrl(String fileKey) async {
-   Map<String, dynamic> variables = {"fileKey": fileKey, "isDownload": false};
-   QueryResult<Object?> response= await _apiService.gqlCall(query:getFileSignUrlQuery,variables: variables);
-   return response.data;
- }
-
-
+  Future<Map<String, dynamic>?> getFileSignUrl(String fileKey) async {
+    Map<String, dynamic> variables = {"fileKey": fileKey, "isDownload": false};
+    QueryResult<Object?> response = await _apiService.gqlCall(
+        query: getFileSignUrlQuery, variables: variables);
+    return response.data;
+  }
 }
